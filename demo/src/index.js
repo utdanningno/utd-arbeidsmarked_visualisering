0a3217df43334d7b11cc32672d3b64eb91ce0798
zimrick <richard@zcreativelabs.com>
2019-08-13 11:41:43 +0200

    Added readme

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from "react"
+import {render} from "react-dom"
 
-import {SmallVisual, LargeVisual} from '../../src'
+import {SmallVisual, LargeVisual} from "../../src"
 
-class Demo extends Component {
-  render() {
-    return <div>
+const Demo = () => {
+  return (
+    <div>
       <h1>Test job-market-visuals Demo</h1>
       <SmallVisual/>
       <LargeVisual/>
     </div>
-  }
+  )
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo/>, document.querySelector("#demo"))
