0e0b284d046e792fd5290d1db32e7515e61aa608
emihul <emil44321@live.no>
2024-05-31 09:48:19 +0200

    Fix: moved demo to src

--- src/App.js
+++ src/App.js
@@ -1,15 +1,278 @@
-import React from 'react';
+
+import React, { useState, useEffect } from "react";
 import { getData, Context2, Context3 } from './index';
 
 function App() {
   return (
     <div>
-      <h1>New page testing</h1>
-      <getData />
-      <Context2 />
-      <Context3 />
+      <Demo/>
     </div>
   );
 }
+
+
+const data = [
+  // "u_bioingeniorfag",
+  // "y_elektriker",
+  // "u_journalistikk",
+  // "y_allmennlege",
+
+  "u_automatiseringsfag",
+  // "u_reservedelsfag",
+  // "u_journalistikk",
+  
+  // "y_journalist",
+  // "y_agronom",
+  // "y_arkitekt",
+  // "y_urolog",
+
+];
+
+const containerStyles = {
+  display: "flex",
+  flexWrap: "wrap",
+  width: "100%",
+  maxWidth: 1120,
+  margin: "0 auto",
+  padding: "2.5rem 0.75rem",
+  boxSizing: "border-box"
+};
+
+const itemStyles = {
+  width: "25%",
+  padding: "0.75rem",
+  boxSizing: "border-box"
+};
+
+const selectStyles = {
+  appearance: "none",
+  height: "2.5rem",
+  fontSize: "inherit",
+  fontFamily: "inherit",
+}
+
+// const disaggregationValues = [
+//   "antall_kvinner",
+//   "antall_menn",
+//   "antall_ukjent_kjonn"
+// ];
+// const disaggregationLabels = ["kvinner", "menn", "ukjent kjonn"];
+
+// const disaggregationValues = ["antall_offentlig", "antall_privat", "antall_ukjent_sektor"]
+// const disaggregationLabels = ["Offentlig", "Privat", "Ukjent sektor"]
+
+// const disaggregationValues = ["antall_13", "antall_710", "other_experience"]
+// const disaggregationLabels = ["1-3 years", "7-10 years", "Other"]
+
+const disaggregationValues = ["antall_40", "over_40"]
+const disaggregationLabels = ["Under 40", "Over 40"]
+
+const disaggregations = [
+  {
+    name: "By age",
+    values: ["antall_40", "over_40"],
+    labels: ["Under 40", "Over 40"],
+  },
+  {
+    name: "By experience",
+    values: ["antall_13", "antall_710", "other_experience"],
+    labels: ["1-3 years", "7-10 years", "Other"],
+  },
+  {
+    name: "By sector",
+    values: ["antall_offentlig", "antall_privat", "antall_ukjent_sektor"],
+    labels: ["Offentlig", "Privat", "Ukjent sektor"],
+  },
+  {
+    name: "By gender",
+    values: ["antall_kvinner", "antall_menn", "antall_ukjent_kjonn"],
+    labels: ["kvinner", "menn", "ukjent kjonn"],
+  }
+]
+
+const Header = ({ title, direction }) => {
+  return (
+    <div style={{ paddingBottom: "1.25rem" }}>
+      <h3 style={{ marginTop: "0", marginBottom: "0.5rem" }}>{ title }</h3>
+      <strong>{ direction }</strong>
+    </div>
+  )
+}
+
+const Context2Demo = ({ layout, disaggregation }) => {
+  const [korts, setKorts] = useState(null)
+  const [nusKortnavn, setNusKortnavn] = useState("all")
+
+  useEffect(() => {
+    getData("y_sykepleier", "uno_id2nus_kortnavn")
+      .then(d => {
+        if (d && d.mapping && d.mapping.docs) setKorts(d.mapping.docs)
+      })
+  }, [])
+
+  return (
+    <div style={{ width: "100%" }}>
+
+      <div style={{ marginBottom: "1.25rem" }}>
+        {
+          korts &&
+          <select
+            style={selectStyles}
+            value={nusKortnavn}
+            onChange={(evt) => setNusKortnavn(evt.target.value)}
+          >
+            <option value="all">{ "All" }</option>
+            {
+              korts.map(d => {
+                const name = d.nus_kortnavn.replace(/"/g, "")
+                return <option key={d.id} value={name}>{name}</option>
+              })
+            }
+          </select>
+        }
+      </div>
+
+      <Context2
+        id={nusKortnavn === "all" ? "y_sykepleier" : nusKortnavn}
+        direction={nusKortnavn === "all" ? "uno_id2styrk08" : "nus_kortnavn2styrk08"}
+        layout={layout}
+        limit={8}
+        disaggregateBy={disaggregation ? disaggregation.values : null}
+        disaggregateLabels={disaggregation ? disaggregation.labels : null}
+      />
+      
+    </div>
+  )
+}
+
+const Demo = () => {
+  const [layout, setLayout] = useState("bars")
+  const [disaggregation, setDisaggregation] = useState(null)
+  return (
+    <div style={containerStyles}>
+
+      <div style={{ position: "sticky", top: 0, zIndex: 999, background: "#FFF", width: "100%" }}>
+        <h1>{ "Controls" }</h1>
+        <button onClick={() => setLayout("bars")}>
+          { "Bars" }
+        </button>
+        <button onClick={() => setLayout("tree")}>
+          { "Tree" }
+        </button>
+        <br />
+        <button onClick={() => setDisaggregation(null)}>
+          { "No disaggregation" }
+        </button>
+        {
+          disaggregations.map(d => 
+            <button key={d.name} onClick={() => setDisaggregation(d)}>
+              { d.name }
+            </button>
+          )
+        }
+      </div>
+
+      <div style={{ width: "100%" }}>
+        <h1>{ "Context 2" }</h1>
+      </div>
+
+      <Context2Demo layout={layout} disaggregation={disaggregation} />
+
+      <div style={{ width: "50%" }}>
+        <Header title="Sykepleie" direction="nus_kortnavn2styrk08" />
+        <Context2
+          id="Sykepleie"
+          direction="nus_kortnavn2styrk08"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+        />
+      </div>
+      <div style={{ width: "50%" }}>
+        <Header title="y_sykepleier" direction="y_* uno_id2styrk08" />
+        <Context2
+          id="y_sykepleier"
+          direction="uno_id2styrk08"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+        />
+      </div>
+      <div style={{ width: "50%" }}>
+        <Header title="u_sykepleie" direction="u_* uno_id2styrk08" />
+        <Context2
+          id="u_sykepleie"
+          direction="uno_id2styrk08"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+        />
+      </div>
+
+
+      <div style={{ width: "100%" }}>
+        <hr style={{ border: 0, borderBottom: "0.25rem solid currentcolor" }} />
+      </div>
+
+      <div style={{ width: "100%" }}>
+        <h1>{ "Context 3" }</h1>
+      </div>
+      <div style={{ width: "25%" }}>
+        <Header title="y_dataingenior" direction="y_* uno_id2styrk08" />
+        <Context3
+          id="y_dataingenior"
+          direction="uno_id2styrk08"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+          moreLabel="Andre yrker"
+        />
+      </div>
+      <div style={{ width: "25%" }}>
+        <Header title="y_dataingenior" direction="y_* uno_id2nus" />
+        <Context3
+          id="y_dataingenior"
+          direction="uno_id2nus"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+          moreLabel="Andre yrker"
+        />
+      </div>
+      <div style={{ width: "25%" }}>
+        <Header title="y_dataingenior" direction="y_* uno_id2nus_kortnavn" />
+        <Context3
+          id="y_dataingenior"
+          direction="uno_id2nus_kortnavn"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+          moreLabel="Andre yrker"
+        />
+      </div>
+      <div style={{ width: "25%" }}>
+        <Header title="u_journalistikk" direction="u_* uno_id2styrk08" />
+        <Context3
+          id="u_journalistikk"
+          direction="uno_id2styrk08"
+          layout={layout}
+          limit={8}
+          disaggregateBy={disaggregation ? disaggregation.values : null}
+          disaggregateLabels={disaggregation ? disaggregation.labels : null}
+          moreLabel="Andre yrker"
+        />
+      </div>
+    </div>
+  )
+}
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ root.render(
 export { default as Context2 } from "./Context2"
 export { default as Context3 } from "./Context3"
 export { getData as getData  }  from "./useData"
+
