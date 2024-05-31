e050a8f5eaf8e95091c2a9e506d5a81fbd2eadad
emihul <emil44321@live.no>
2024-05-31 09:41:20 +0200

    Fix: changed from nwb to react-script

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { getData, Context2, Context3 } from './index';
+
+function App() {
+  return (
+    <div>
+      <h1>New page testing</h1>
+      <getData />
+      <Context2 />
+      <Context3 />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
 export { default as Context2 } from "./Context2"
 export { default as Context3 } from "./Context3"
 export { getData as getData  }  from "./useData"
