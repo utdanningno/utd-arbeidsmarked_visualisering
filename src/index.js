b65f00e616adaf9b7b079406e9f95d22fc90a3fc
emihul <emil44321@live.no>
2024-06-10 14:52:42 +0200

    Fix: Changed from createRoot to reactDom.render

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createRoot } from 'react-dom/client';
 
-const domNode = document.getElementById('root');
-const root = createRoot(domNode);
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
