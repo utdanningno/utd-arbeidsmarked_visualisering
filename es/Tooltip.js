bc3175b4e693272b0119439c9e183e1004ff5126
emihul <emil44321@live.no>
2025-01-10 13:14:11 +0100

    build

--- es/Tooltip.js
+++ es/Tooltip.js
@@ -9,6 +9,11 @@ var _propTypes = _interopRequireDefault(require("prop-types"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 function _getRequireWildcardCache(e) { if ("function" != typeof WeakMap) return null; var r = new WeakMap(), t = new WeakMap(); return (_getRequireWildcardCache = function (e) { return e ? t : r; })(e); }
 function _interopRequireWildcard(e, r) { if (!r && e && e.__esModule) return e; if (null === e || "object" != typeof e && "function" != typeof e) return { default: e }; var t = _getRequireWildcardCache(r); if (t && t.has(e)) return t.get(e); var n = { __proto__: null }, a = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var u in e) if ("default" !== u && {}.hasOwnProperty.call(e, u)) { var i = a ? Object.getOwnPropertyDescriptor(e, u) : null; i && (i.get || i.set) ? Object.defineProperty(n, u, i) : n[u] = e[u]; } return n.default = e, t && t.set(e, n), n; }
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 const Tooltip = _ref => {
   let {
     tooltip,
@@ -16,7 +21,7 @@ const Tooltip = _ref => {
     tooltipCaretStyles
   } = _ref;
   return tooltip.item ? /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement("div", {
-    style: {
+    style: _objectSpread({
       display: tooltip.visible ? "block" : "none",
       position: "fixed",
       left: tooltip.position[0],
@@ -31,16 +36,15 @@ const Tooltip = _ref => {
       borderRadius: "0.25rem",
       fontSize: 14,
       overflow: "hidden",
-      boxSizing: "border-box",
-      ...tooltipStyles
-    }
+      boxSizing: "border-box"
+    }, tooltipStyles)
   }, tooltip.item.customContent ? /*#__PURE__*/_react.default.createElement(_react.Fragment, null, tooltip.item.customContent) : /*#__PURE__*/_react.default.createElement(_react.Fragment, null, /*#__PURE__*/_react.default.createElement("span", {
     style: {
       display: "block",
       overflow: "hidden"
     }
   }, tooltip.item.title), tooltip.item.number)), /*#__PURE__*/_react.default.createElement("div", {
-    style: {
+    style: _objectSpread({
       display: tooltip.visible ? "block" : "none",
       position: "fixed",
       left: tooltip.caretPosition[0],
@@ -54,9 +58,8 @@ const Tooltip = _ref => {
       borderRight: "0.5rem solid transparent",
       borderTop: "0.5rem solid rgba(0,0,0,0.8)",
       borderLeft: "0.5rem solid transparent",
-      boxSizing: "border-box",
-      ...tooltipCaretStyles
-    }
+      boxSizing: "border-box"
+    }, tooltipCaretStyles)
   })) : null;
 };
 Tooltip.propTypes = {
--- es/useData.js
+++ es/useData.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", {
 exports.getData = getData;
 exports.useData = useData;
 var _react = require("react");
+function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
+function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
+function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == typeof i ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != typeof t || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != typeof i) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 async function getData(id, direction) {
   let api_url = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'https://api.utdanning.no';
   const mainEndpoint = api_url + '/sammenligning/main';
@@ -33,15 +38,13 @@ async function getData(id, direction) {
   /**
    * 3. Add missing fields
    */
-  const docs = mapping.response && mapping.response.docs ? mapping.response.docs.map(d => ({
-    ...d,
+  const docs = mapping.response && mapping.response.docs ? mapping.response.docs.map(d => _objectSpread(_objectSpread({}, d), {}, {
     over_40: d.antall_personer - d.antall_40,
     other_experience: d.antall_personer - d.antall_13 - d.antall_710
   })) : null;
-  const mappingData = mapping.response ? {
-    ...mapping.response,
+  const mappingData = mapping.response ? _objectSpread(_objectSpread({}, mapping.response), {}, {
     docs
-  } : null;
+  }) : null;
   return {
     main: main ? main[id] : null,
     mapping: mappingData
