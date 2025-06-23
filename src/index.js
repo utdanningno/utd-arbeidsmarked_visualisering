0d9473d00a9b4d034c6dafdc08309edf3c35a289
Harald Berge <harald.berge@hkdir.no>
2025-06-23 09:43:21 +0200

    UTDTEK-4027 Sjekk at root eksisterer før vi bruker container

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
-
+if (container) {
+  const root = createRoot(container); // createRoot(container!) if you use TypeScript
+  root.render(<App />);
+}
 export { default as Context2 } from './Context2';
 export { default as Context3 } from './Context3';
 export { getData as getData } from './useData';
