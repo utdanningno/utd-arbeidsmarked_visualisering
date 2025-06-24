e3581c41ddfa024c9bc14762c5f334bac8126f74
Harald Berge <harald.berge@hkdir.no>
2025-06-24 15:14:44 +0200

    Lagt inn if-test på root

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
 
 export { default as Context2 } from "./Context2"
 export { default as Context3 } from "./Context3"
