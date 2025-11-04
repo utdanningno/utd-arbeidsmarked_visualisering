4b8940e1d5a30e75cb13fec2efe4910fb12bde37
Ben <benhuy2010@yahoo.com>
2025-11-04 13:36:41 +0100

    øke avstand mellom punktum og lenke

--- es/SourceInfo.js
+++ es/SourceInfo.js
@@ -22,7 +22,7 @@ const SourceInfo = _ref => {
   const dato = new Date(Date.parse(kildedato));
   const year = dato.getFullYear();
   const month = _utils.months[dato.getMonth()];
-  return /*#__PURE__*/_react.default.createElement("p", null, "Kilde: Statistisk sentralbyr\xE5 ", year + 1, ". Tall fra ", month, " ", year, ".", /*#__PURE__*/_react.default.createElement("a", {
+  return /*#__PURE__*/_react.default.createElement("p", null, "Kilde: Statistisk sentralbyr\xE5 ", year + 1, ". Tall fra ", month, " ", year, ".", " ", /*#__PURE__*/_react.default.createElement("a", {
     href: "/tema/om_utdanning.no/om_yrkes-_og_utdanningstatistikken"
   }, "Forklaring til statistikken"));
 };
